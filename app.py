dcaca5d9660b6c5a3266fdcd45971b81f10d2231
Shiv Dixit <kaccatur@Shivs-MacBook-Pro.local>
2025-01-17 10:36:22 +0530

    updates

--- app.py
+++ app.py
@@ -2,38 +2,46 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from tensorflow.keras.models import load_model
-print('imported')
+import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-# Load the model
-model = load_model('next_word_lstm.h5')
+# Load the TensorFlow Lite model
+interpreter = tf.lite.Interpreter(model_path="quantized_model.tflite")
+interpreter.allocate_tensors()
 
 # Load the tokenizer
 with open('tokenizer.pickle', 'rb') as f:
     tokenizer = pickle.load(f)
 
+# Get input and output details for the TFLite model
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-## function to predict the next word
-
-def predict_next_word(model,tokenizer, text,max_sequence_len):
+## Function to predict the next word using TensorFlow Lite model
+def predict_next_word_tflite(interpreter, tokenizer, text, max_sequence_len):
     token_list = tokenizer.texts_to_sequences([text])[0]
     if len(token_list) >= max_sequence_len:
-        token_list = token_list[-(max_sequence_len-1):] ## take the last max_sequence_len-1 tokens
-    token_list = pad_sequences([token_list],maxlen=max_sequence_len-1,padding='pre') ## pad the sequence
-    predicted = model.predict(token_list, verbose=0) ## predict the next token
-    predict_next_word = np.argmax(predicted,axis=1) ## get the index of the next token
-
-    for word,index in tokenizer.word_index.items():
-        if index == predict_next_word:
+        token_list = token_list[-(max_sequence_len-1):]  # Take the last max_sequence_len-1 tokens
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')  # Pad the sequence
+    
+    # Set the input tensor
+    interpreter.set_tensor(input_details[0]['index'], token_list.astype(np.float32))
+    interpreter.invoke()
+    
+    # Get the output tensor
+    predicted = interpreter.get_tensor(output_details[0]['index'])
+    predict_next_word_idx = np.argmax(predicted, axis=1)  # Get the index of the next token
+    
+    for word, index in tokenizer.word_index.items():
+        if index == predict_next_word_idx:
             return word
     return None
 
-## streamlit app
+## Streamlit app
 st.title('Next Word Prediction')
 
 input_text = st.text_input('Enter the sequence of words:', "To be or not to")
 if st.button('Predict Next Word'):
-    max_sequence_len = model.input_shape[1] + 1
-    next_word = predict_next_word(model,tokenizer,input_text,max_sequence_len)
+    max_sequence_len = input_details[0]['shape'][1] + 1  # Get max_sequence_len from the TFLite model input shape
+    next_word = predict_next_word_tflite(interpreter, tokenizer, input_text, max_sequence_len)
     st.write(f'The next word is: {next_word}')
